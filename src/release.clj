e0ccd86df2e5b8453513049b87a4fcb47618b004
Madis Nõmme <me@mad.is>
2022-12-12 07:22:49 -0600

    Don't use :local-deps alias for POM basis
    At first tried to use local dependencies (in relative paths) during POM
    generation. This doesn't work because Maven needs real versions and by
    referencing local repos via relative paths, these artefacts get excluded
    from the POM, because nobody else (except the local machine) has any
    information about them.
    
    So during POM generation real, published versions must be used

--- src/release.clj
+++ src/release.clj
@@ -21,8 +21,7 @@
     (b/write-pom {:class-dir class-dir
                   :lib artefact-id
                   :version version
-                  :basis (b/create-basis {:project (subdir (first lib-paths) "deps.edn")
-                                          :aliases [:local-deps]})
+                  :basis (b/create-basis {:project (subdir (first lib-paths) "deps.edn")})
                   :src-dirs src-dirs})
     (b/copy-dir {:src-dirs src-dirs
                  :target-dir class-dir})
